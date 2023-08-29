46ad518663ba3a61dfa49ed6a3e5e615872969ca
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2023-08-28 23:39:02 -0700

    Remove debugging

--- src/routes/[name]/+page.server.ts
+++ src/routes/[name]/+page.server.ts
@@ -3,27 +3,29 @@ import { Hypicle, Player } from 'hypicle';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params }) => {
-	const client = new Hypicle(API_KEY);
-	const uuid = await getUUIDByName(params.name);
-	console.log(`UUID of player ${params.name} is ${uuid}`);
-	const player = new Player(client, uuid);
+	try {
+		const client = new Hypicle(API_KEY);
+		const uuid = await getUUIDByName(params.name);
+		const player = new Player(client, uuid);
 
-	const stats = player.getStats();
-	const bedwarsStats = await stats.getBedwars().get();
+		const stats = player.getStats();
+		const bedwarsStats = await stats.getBedwars().get();
 
-	return {
-		name: params.name,
-		bedwars: bedwarsStats
-	};
+		return {
+			name: params.name,
+			bedwars: bedwarsStats
+		};
+	} catch (error) {
+		console.error(error);
+		return {
+			name: 'Not found',
+			bedwars: null
+		};
+	}
 };
 
 async function getUUIDByName(name: string) {
 	const response = await fetch(`https://playerdb.co/api/player/minecraft/${name}`);
-	console.log('status is', response.status);
-	console.log('headers are', response.headers);
-	console.log('response is', response);
 	const json = await response.json();
-	console.log('mojang json is', json);
 	return json.data.player.id as string;
-	// return (await response.json()).id as string;
 }
