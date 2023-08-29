b49a5fd86447b6b50b4fcad1eb4b2fb225ed6564
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2023-08-28 23:04:27 -0700

    Use page server load fetch

--- src/routes/[name]/+page.server.ts
+++ src/routes/[name]/+page.server.ts
@@ -2,28 +2,25 @@ import { API_KEY } from '$env/static/private';
 import { Hypicle, Player } from 'hypicle';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ params }) => {
-	try {
-		const client = new Hypicle(API_KEY);
-		const uuid = await getUUIDByName(params.name);
-		const player = new Player(client, uuid);
+export const load: PageServerLoad = async ({ params, fetch }) => {
+	const client = new Hypicle(API_KEY);
+	const uuid = await getUUIDByName(params.name, fetch);
+	console.log(`UUID of player ${params.name} is ${uuid}`);
+	const player = new Player(client, uuid);
 
-		const stats = player.getStats();
-		const bedwarsStats = await stats.getBedwars().get();
+	const stats = player.getStats();
+	const bedwarsStats = await stats.getBedwars().get();
 
-		return {
-			name: params.name,
-			bedwars: bedwarsStats
-		};
-	} catch (error) {
-		return {
-			name: 'Not found',
-			bedwars: null
-		};
-	}
+	return {
+		name: params.name,
+		bedwars: bedwarsStats
+	};
 };
 
-async function getUUIDByName(name: string) {
+async function getUUIDByName(
+	name: string,
+	fetch: (input: RequestInfo | URL, init?: RequestInit | undefined) => Promise<Response>
+) {
 	const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${name}`);
 	return (await response.json()).id as string;
 }
