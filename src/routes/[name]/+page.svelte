8d122bfb753a471d039c31bca0b7f602879cc427
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2023-09-25 18:30:18 -0700

    More status I guess

--- src/routes/[name]/+page.svelte
+++ src/routes/[name]/+page.svelte
@@ -5,6 +5,18 @@
 
 	// @ts-expect-error
 	const armedBedsBroken: number = data.bedwars?.four_four_armed_beds_broken_bedwars;
+
+	function decipherRecord(name: string) {
+		// example record bridging_distance_30:elevation_NONE:angle_STRAIGHT:
+		name = name.toLowerCase().replaceAll('_', ' ').replaceAll(':', ', ').trim();
+		// get rid of last comma
+		name = name.slice(0, -1);
+
+		// idk what copilot did here
+		name = name.replace(/\w\S*/g, (w) => w.replace(/^\w/, (c) => c.toUpperCase()));
+
+		return name;
+	}
 </script>
 
 <div class="space-y-4">
@@ -13,6 +25,10 @@
 		{@const bw = data.bedwars}
 		{@const voidKDR = bw.void_kills_bedwars / bw.void_deaths_bedwars}
 		{@const voidFKDR = bw.void_final_kills_bedwars / bw.void_final_deaths_bedwars}
+
+		<div>
+			<Stat name="Experience" value={bw.Experience} />
+		</div>
 
 		<div>
 			<Stat name="Void Kills" value={bw.void_kills_bedwars} />
@@ -30,6 +46,33 @@
 			<Stat name="Armed Beds Broken" value={armedBedsBroken} />
 			<Stat name="Armed Beds Lost" value={bw.four_four_armed_beds_lost_bedwars} />
 		</div>
+
+		<div>
+			<Stat
+				name="Doubles Explosion Final Deaths"
+				value={bw.eight_two_entity_explosion_final_deaths_bedwars}
+			/>
+		</div>
+
+		{#if bw.practice}
+			<div>
+				<Stat
+					name="Blocks Placed in Bridging Practice"
+					value={bw.practice.bridging.blocks_placed}
+				/>
+				<Stat
+					name="Blocks Placed in Fireball Jumping Practice"
+					value={bw.practice.fireball_jumping.blocks_placed}
+				/>
+			</div>
+
+			<div>
+				{#each Object.keys(bw.practice.records) as key}
+					{@const record = bw.practice.records[key]}
+					<Stat name={decipherRecord(key)} value={record / 1000 + ' seconds'} />
+				{/each}
+			</div>
+		{/if}
 	{:else}
 		<p>Bedwars stats not found</p>
 	{/if}
